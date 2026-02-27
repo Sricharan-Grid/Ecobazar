7a090e09727e85b002e7f640e0e07c086b0835d9
Sricharan-Grid <snanthakumaran@griddynamics.com>
2026-02-27 16:50:54 +0530

    Implemented Hamburger Menu

--- templates/scripts/script.js
+++ templates/scripts/script.js
@@ -14,8 +14,21 @@ const offerTimer = (() => {
     const minutesPending = Math.floor((diffMs % (60000 * 60)) / 60000);
     const secondsPending = Math.floor((diffMs % (1000 * 60)) / 1000);
 
-    hoursEl.textContent = (hoursPending);
+    hoursEl.textContent = hoursPending;
     minutesEl.textContent = String(minutesPending).padStart(2, "0");
     secondsEl.textContent = String(secondsPending).padStart(2, "0");
   }, 1000);
 })();
+
+//Hamburger menu
+
+const hamburger = document.querySelector(".hamburger-menu");
+const hamburgerBtn = document.querySelector(".hamburger-menu__bar");
+const hamburgerOptions = document.querySelector('.hamburger-menu__options');
+
+hamburgerBtn.addEventListener("click", () => {
+  // hamburgerBtn.classList.remove("hamburger-menu__bar");
+  console.log('hamburger Clicked')
+  hamburgerBtn.classList.toggle("hamburger-menu__bar--close");
+  hamburgerOptions.classList.toggle('hamburger-hidden');
+});
